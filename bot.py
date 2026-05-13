5bdca13cac05ef1fc45c15a61a9a9154edcecba7
mdridoy3999-lang <md.ridoy3999@gmail.com>
2026-05-13 12:12:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,31 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    MessageHandler,
+    filters,
+    ContextTypes
+)
 
 TOKEN = "8409479235:AAGBBODhZBQyKf76-zKevURrxHzYM4nINOA"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot Online 😄")
+    await update.message.reply_text("আসসালামু আলাইকুম 😄")
+
+async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_text = update.message.text
+
+    await update.message.reply_text(
+        f"তুমি বলেছো: {user_text}"
+    )
 
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
+
+app.add_handler(
+    MessageHandler(filters.TEXT & ~filters.COMMAND, reply)
+)
 
 print("Bot Running...")
 app.run_polling()
