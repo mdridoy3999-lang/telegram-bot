283015a837ad428728056ee65e58f11fdb18a06d
mdridoy3999-lang <md.ridoy3999@gmail.com>
2026-05-15 23:26:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,41 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+)
+
 import random
 
 TOKEN = "8409479235:AAGBBODhZBQyKf76-zKevURrxHzYM4nINOA"
 
 pairs = [
-    "USD/NGN",
     "EUR/USD",
     "GBP/USD",
     "USD/JPY",
-    "USD/BDT",
-    "USD/BRL",
-    "NZD/CAD",
-    "CAD/CHF",
-    "USD/PKR"
+    "USD/CAD",
+    "AUD/USD",
+    "NZD/USD",
+    "EUR/JPY",
+    "GBP/JPY",
+    "EUR/USD OTC",
+    "GBP/USD OTC",
+    "USD/JPY OTC",
+    "AUD/USD OTC",
 ]
 
 directions = [
     "⬆️ UP",
-    "🔽 DOWN"
+    "⬇️ DOWN"
 ]
 
+# START COMMAND
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🤖 Signal Bot Online")
+    await update.message.reply_text(
+        "🤖 Trading Signal Bot Online 🔥"
+    )
 
+# SIGNAL COMMAND
 async def signal(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     pair = random.choice(pairs)
@@ -38,10 +50,12 @@ async def signal(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await update.message.reply_text(msg)
 
+# MAIN BOT
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("signal", signal))
 
 print("Bot Running...")
+
 app.run_polling()
